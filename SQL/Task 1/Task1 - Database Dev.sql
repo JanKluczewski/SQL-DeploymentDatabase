b3ae189bc226cb91a6da88edbb8e327b6f9fb3fa
JanKluczewski <kluczewski.jan.leonard@gmail.com>
2025-11-13 16:03:31 +0100

    task1 and task 2

---
+++ SQL/Task 1/Task1 - Database Dev.sql
@@ -0,0 +1,44 @@
+-- =========================================
+-- Skrypt: Utworzenie baz DeploymentCore_DEV i DeploymentCore_TST
+-- Autor: Jan Kluczewski
+-- System: Microsoft SQL Server (MSSQL)
+-- =========================================
+
+-- Ustaw kontekst na master, bo nie możemy tworzyć bazy z innej bazy
+USE [master];
+GO
+
+-- =========================================
+-- Tworzenie bazy DeploymentCore_DEV
+-- =========================================
+IF NOT EXISTS (
+    SELECT name 
+    FROM sys.databases 
+    WHERE name = N'DeploymentCore_DEV'
+)
+BEGIN
+    PRINT 'Tworzenie bazy DeploymentCore_DEV...';
+    
+    CREATE DATABASE [DeploymentCore_DEV]
+    ON PRIMARY (
+        NAME = N'DeploymentCore_DEV_Data',
+        FILENAME = N'C:\SQLData\DeploymentCore_DEV.mdf',
+        SIZE = 50MB,
+        MAXSIZE = UNLIMITED,
+        FILEGROWTH = 10MB
+    )
+    LOG ON (
+        NAME = N'DeploymentCore_DEV_Log',
+        FILENAME = N'C:\SQLData\DeploymentCore_DEV_log.ldf',
+        SIZE = 20MB,
+        MAXSIZE = UNLIMITED,
+        FILEGROWTH = 10MB
+    );
+
+    PRINT 'Baza DeploymentCore_DEV została utworzona.';
+END
+ELSE
+BEGIN
+    PRINT 'Baza DeploymentCore_DEV już istnieje.';
+END
+GO
---
+++ SQL/Task 2/Task2 - Database Tst.sql
@@ -0,0 +1,44 @@
+-- =========================================
+-- Skrypt: Utworzenie baz DeploymentCore_DEV i DeploymentCore_TST
+-- Autor: Jan Kluczewski
+-- System: Microsoft SQL Server (MSSQL)
+-- =========================================
+
+-- Ustaw kontekst na master, bo nie możemy tworzyć bazy z innej bazy
+USE [master];
+GO
+
+-- =========================================
+-- Tworzenie bazy DeploymentCore_TST
+-- =========================================
+IF NOT EXISTS (
+    SELECT name 
+    FROM sys.databases 
+    WHERE name = N'DeploymentCore_TST'
+)
+BEGIN
+    PRINT 'Tworzenie bazy DeploymentCore_TST...';
+    
+    CREATE DATABASE [DeploymentCore_TST]
+    ON PRIMARY (
+        NAME = N'DeploymentCore_TST_Data',
+        FILENAME = N'C:\SQLData\DeploymentCore_TST.mdf',
+        SIZE = 50MB,
+        MAXSIZE = UNLIMITED,
+        FILEGROWTH = 10MB
+    )
+    LOG ON (
+        NAME = N'DeploymentCore_TST_Log',
+        FILENAME = N'C:\SQLData\DeploymentCore_TST_log.ldf',
+        SIZE = 20MB,
+        MAXSIZE = UNLIMITED,
+        FILEGROWTH = 10MB
+    );
+
+    PRINT 'Baza DeploymentCore_TST została utworzona.';
+END
+ELSE
+BEGIN
+    PRINT 'Baza DeploymentCore_TST już istnieje.';
+END
+GO
