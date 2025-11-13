19444a13b7a5316c0454e683889a3a120cfb66ac
JanKluczewski <kluczewski.jan.leonard@gmail.com>
2025-11-13 16:59:00 +0100

    fix script

--- SQL/Task1/Task1 - Database Dev.sql
+++ SQL/Task1/Task1 - Database Dev.sql
@@ -9,36 +9,36 @@ USE [master];
 GO
 
 -- =========================================
--- Tworzenie bazy DeploymentCore_TST
+-- Tworzenie bazy DeploymentCore_DEV
 -- =========================================
 IF NOT EXISTS (
     SELECT name 
     FROM sys.databases 
-    WHERE name = N'DeploymentCore_TST'
+    WHERE name = N'DeploymentCore_DEV'
 )
 BEGIN
-    PRINT 'Tworzenie bazy DeploymentCore_TST...';
+    PRINT 'Tworzenie bazy DeploymentCore_DEV...';
     
-    CREATE DATABASE [DeploymentCore_TST]
+    CREATE DATABASE [DeploymentCore_DEV]
     ON PRIMARY (
-        NAME = N'DeploymentCore_TST_Data',
-        FILENAME = N'C:\SQLData\DeploymentCore_TST.mdf',
+        NAME = N'DeploymentCore_DEV_Data',
+        FILENAME = N'/var/opt/mssql/data1/DeploymentCore_DEV.mdf',
         SIZE = 50MB,
         MAXSIZE = UNLIMITED,
         FILEGROWTH = 10MB
     )
     LOG ON (
-        NAME = N'DeploymentCore_TST_Log',
-        FILENAME = N'C:\SQLData\DeploymentCore_TST_log.ldf',
+        NAME = N'DeploymentCore_DEV_Log',
+        FILENAME = N'/var/opt/mssql/data1/DeploymentCore_DEV_log.ldf',
         SIZE = 20MB,
         MAXSIZE = UNLIMITED,
         FILEGROWTH = 10MB
     );
 
-    PRINT 'Baza DeploymentCore_TST została utworzona.';
+    PRINT 'Baza DeploymentCore_DEV została utworzona.';
 END
 ELSE
 BEGIN
-    PRINT 'Baza DeploymentCore_TST już istnieje.';
+    PRINT 'Baza DeploymentCore_DEV już istnieje.';
 END
 GO
--- SQL/Task2/Task2 - Database Tst.sql
+++ SQL/Task2/Task2 - Database Tst.sql
@@ -9,36 +9,36 @@ USE [master];
 GO
 
 -- =========================================
--- Tworzenie bazy DeploymentCore_DEV
+-- Tworzenie bazy DeploymentCore_TST
 -- =========================================
 IF NOT EXISTS (
     SELECT name 
     FROM sys.databases 
-    WHERE name = N'DeploymentCore_DEV'
+    WHERE name = N'DeploymentCore_TST'
 )
 BEGIN
-    PRINT 'Tworzenie bazy DeploymentCore_DEV...';
+    PRINT 'Tworzenie bazy DeploymentCore_TST...';
     
-    CREATE DATABASE [DeploymentCore_DEV]
+    CREATE DATABASE [DeploymentCore_TST]
     ON PRIMARY (
-        NAME = N'DeploymentCore_DEV_Data',
-        FILENAME = N'C:\SQLData\DeploymentCore_DEV.mdf',
+        NAME = N'DeploymentCore_TST_Data',
+        FILENAME = N'/var/opt/mssql/data2/eploymentCore_TST.mdf',
         SIZE = 50MB,
         MAXSIZE = UNLIMITED,
         FILEGROWTH = 10MB
     )
     LOG ON (
-        NAME = N'DeploymentCore_DEV_Log',
-        FILENAME = N'C:\SQLData\DeploymentCore_DEV_log.ldf',
+        NAME = N'DeploymentCore_TST_Log',
+        FILENAME = N'/var/opt/mssql/data2/eploymentCore_TST_log.ldf',
         SIZE = 20MB,
         MAXSIZE = UNLIMITED,
         FILEGROWTH = 10MB
     );
 
-    PRINT 'Baza DeploymentCore_DEV została utworzona.';
+    PRINT 'Baza DeploymentCore_TST została utworzona.';
 END
 ELSE
 BEGIN
-    PRINT 'Baza DeploymentCore_DEV już istnieje.';
+    PRINT 'Baza DeploymentCore_TST już istnieje.';
 END
 GO
